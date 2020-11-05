900f1c24d34fda9543c055ff0348ef72535ef96a
Tolga Kara <tolgakara2.0@gmail.com>
2020-11-05 09:29:34 +0100

    Integrated Phantom JS

--- script.py
+++ script.py
@@ -1,4 +1,8 @@
+import secrets
 from selenium import webdriver
+driver = webdriver.PhantomJS("""D:/Programme/phantomjs-2.1.1-windows/bin/phantomjs.exe""")
+driver.set_window_size(1120, 550)
+
 
 def write_resume(data):
   """
@@ -30,11 +34,18 @@ def get_job_posting_from_stepstone(url):
   docstring
   """
   jobplatform_provider="StepStone"
-  pass
+  driver.get("https://duckduckgo.com/")
+  driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+  driver.find_element_by_id("search_button_homepage").click()
+  print(driver.current_url)
+  driver.quit()
 
 
 if __name__ == "__main__":
+  print(secrets.LINKEDIN_USERNAME)
   counter = 10
+  get_job_posting_from_stepstone('')
+
   for counter in range(0,counter):
-    print('counter')
+    pass
 
