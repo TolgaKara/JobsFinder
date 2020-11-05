13e64d2bc495cc382d3acfc01fa02e9022daacf9
Tolga Kara <tolgakara2.0@gmail.com>
2020-11-05 09:04:53 +0100

    Some simple boiler code

--- script.py
+++ script.py
@@ -1,2 +1,40 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
+def write_resume(data):
+  """
+  docstring
+  """
+  pass
+
+def write_cover_letter(data):
+  """
+  docstring
+  """
+  pass
+
+def write_job_posting_summary(data):
+  """
+  docstring
+  """
+  pass
+
+def get_job_posting_from_linkedin(url):
+  """
+  docstring
+  """
+  jobplatform_provider="LinkedIn"
+  pass
+
+def get_job_posting_from_stepstone(url):
+  """
+  docstring
+  """
+  jobplatform_provider="StepStone"
+  pass
+
+
+if __name__ == "__main__":
+  counter = 10
+  for counter in range(0,counter):
+    print('counter')
+
